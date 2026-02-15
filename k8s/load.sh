b53e9639b3c2234b34ef7d6363b087d154e6a774
mrflick72 <valerio.vaudi@gmail.com>
2026-02-15 13:07:02 +0100

    prometheus endpoint port fix

--- k8s/load.sh
+++ k8s/load.sh
@@ -1,6 +1,6 @@
 while [ true ]; do
   echo $(date) >> result.txt
-  echo $(curl http://grpc.hello-service.local/hello/valerio )>> result.txt
+  echo $(curl http://172.25.255.200:8085/hello/valerio )>> result.txt
 
   sleep 0.2
 done
